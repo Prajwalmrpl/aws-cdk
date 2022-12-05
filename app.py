7065f63ae2354d0856b4b3dda52bd0b552ece5da
Prajwal <prajwal@Prajwals-MacBook-Pro.local>
2022-12-05 16:38:28 +0530

    added stacks

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ import os
 
 import aws_cdk as cdk
 
-from day_8.day_8_stack import Day8Stack
-from day_8.s3_with_cloudfront import DeployCloudfrontOaiStaticSiteStack
-from day_8.static_with_contents import DeployStaticSiteStack
+from day_7.day_7_stack import Day7Stack
+from day_7.ec2_with_alaram import CustomEc2WithAlarmsStack
+from day_7.cw_metrics import CustomEc2WithAlarmsStack
+from day_7.cw_live_dashboard import CustomCloudwatchLiveDashboardStack
 
 
 app = cdk.App()
 Day8Stack(app, "Day8Stack",)
 
-DeployCloudfrontOaiStaticSiteStack(app, "DeployCloudfrontOaiStaticSiteStack", description="Stack to DeployCloudfrontOaiStaticSiteStack")
+deploy_cloud_st = DeployCloudfrontOaiStaticSiteStack(app, "DeployCloudfrontOaiStaticSiteStack", description="Stack to DeployCloudfrontOaiStaticSiteStack")
+cdk.Tags.of(deploy_cloud_st).add("Name", "CfnStack")
 
 DeployStaticSiteStack(app, "DeployStaticSiteStack", description="Stack for DeployStaticSiteStack")
 
